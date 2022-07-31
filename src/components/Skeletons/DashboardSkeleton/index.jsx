e8708a5c3c223e404dea570a5babf982fec4b68b
John Daniels <adeyemijohndaniels@gmial.com>
2022-07-31 16:12:04 +0100

    styled the skeleton a little - finalizing

--- src/components/Skeletons/DashboardSkeleton/index.jsx
+++ src/components/Skeletons/DashboardSkeleton/index.jsx
@@ -1,6 +1,20 @@
 import Skeleton from "@mui/material/Skeleton"
+import { useState, useEffect } from "react"
 
 export default function DashboardSkeleton({ tabs, boxes }) {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+  useEffect(() => {
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth)
+    }
+
+    window.addEventListener("resize", changeWidth)
+
+    return () => {
+      window.removeEventListener("resize", changeWidth)
+    }
+  }, []) // not really needed
+
   const Nav = () => (
     <nav
       className='navigation-dashboard bg-[#fff]'
@@ -31,30 +45,25 @@ export default function DashboardSkeleton({ tabs, boxes }) {
     <>
       <Nav />
       <main className='w-full h-auto bg-[#fafafa] p-[20px] flex'>
-        <div className='w-[350px] mr-[3rem] min-h-screen rounded-[10px] bg-[#ffffff] py-[3rem] left-nav'>
-          <div className='w-full h-auto grid grid-cols-1'>
-            {tabs.map((t, index) => (
-              <TabTitle key={index} />
-            ))}
+        {screenWidth > 1000 && (
+          <div className='w-[350px] mr-[3rem] min-h-screen rounded-[10px] bg-[#ffffff] py-[3rem] left-nav'>
+            <div className='w-full h-auto grid grid-cols-1'>
+              {tabs.map((t, index) => (
+                <TabTitle key={index} />
+              ))}
+            </div>
+            <div className='border-t-[1px] border-[#c4c4c47b] h-[200px] mt-[5rem] flex items-center'>
+              <Skeleton
+                variant='text'
+                height={100}
+                className='w-full px-[20px] py-[17px] cursor-pointer  hover:bg-[#c8c8c83a]'
+              />
+            </div>
           </div>
-          <div className='border-t-[1px] border-[#c4c4c47b] h-[200px] mt-[5rem] flex items-center'>
-            <Skeleton
-              variant='text'
-              height={100}
-              className='w-full px-[20px] py-[17px] cursor-pointer  hover:bg-[#c8c8c83a]'
-            />
-          </div>
-        </div>
+        )}
 
         {/* content */}
         <MainSkeleton boxes={boxes} />
-
-        {/* <Routes>
-          <Route path='/*' element={<Dashboard user={user} />} />
-          <Route path='/submit-questions' element={<SubmitQuestions />} />
-          <Route path='/feedback' element={<Feedback />} />
-          <Route path='/search-questions' element={<SearchQuestions />} />
-        </Routes> */}
       </main>
     </>
   )
@@ -64,7 +73,7 @@ const MainSkeleton = ({ boxes }) => (
   <>
     {/* // show a skeleton here */}
 
-    <div className='w-full h-screen grid grid-cols-3 gap-x-[50px] py-[3rem] px-[1rem]'>
+    <div className='w-full h-[600px] px-[1rem] dashboard-container-home admin-dashboard__Content'>
       {boxes.map((box, index) => (
         <div key={index}>
           <Skeleton
